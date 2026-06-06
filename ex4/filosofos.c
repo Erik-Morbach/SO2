b6d922bbc82c7887827975385b6c491c10b5e5e3
Erik Morbach <morbacherik@gmail.com>
2026-06-06 17:20:02 -0300

    implementação base

---
+++ ex4/filosofos.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define N 5
+#define LEFT (i+N-1)%N
+#define RIGHT (i+1)%N
+#define THINKING 0
+#define HUNGRY 1
+#define EATING 2
+
+int state[N], i, int_rand;
+float float_rand;
+
+void mostrar(void);
+void pensar(int);
+void pegar_garfo(int);
+void por_garfo(int);
+void comer(int);
+void test(int);
+void *acao_filosofo(void *);
+
+sem_t* mutex;
+sem_t* sem_fil[N];
+int main(){
+	for(int i=0;i<N;i++) {
+		state[i] = THINKING;
+	}
+	mostrar();
+
+	int res;
+	pthread_t thread[N];
+	void *thread_result;
+	mutex = sem_open("/mutex", O_CREAT, 0644, 1);
+	for(i=0; i<N; i++){
+		char name[20];
+		sprintf(name, "/sem_fil_%d", i);
+		sem_fil[i] = sem_open(name, O_CREAT, 0644, 0);
+	}
+
+	for(i=0; i<N; i++){
+		res = pthread_create(&thread[i],NULL,acao_filosofo,&i);
+		if(res!=0){
+			perror("Erro na inicialização da thread!");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	for(i=0; i<N; i++){
+        res = pthread_join(thread[i],&thread_result);
+        if(res!=0){
+            perror("Erro ao fazer join nas threads!");
+            exit(EXIT_FAILURE);
+        }
+    }
+    return 0;
+}
+
+void estatistica() {
+
+}
+
+void mostrar(){
+    for(i=1; i<=N; i++){
+        if(state[i-1] == THINKING){
+            printf("O filosofo %d esta pensando!\n", i);
+        }
+        if(state[i-1] == HUNGRY){
+            printf("O filosofo %d esta com fome!\n", i);
+        }
+        if(state[i-1] == EATING){
+            printf("O filosofo %d esta comendo!\n", i);
+        }
+    }
+    printf("\n");
+}
+void *acao_filosofo(void *j){
+    int i = *(int*) j;
+    while(1){
+        pensar(i);
+        pegar_garfo(i);
+        comer(i);
+        por_garfo(i);
+    }
+}
+void pensar(int i){
+    float_rand=0.001*random();
+    int_rand=float_rand;
+    usleep(int_rand);
+}
+void pegar_garfo(int i){
+    sem_wait(mutex);
+    state[i]=HUNGRY;
+    mostrar();
+    test(i);
+    sem_post(mutex);
+    sem_wait(sem_fil[i]);
+}
+void test(int i){
+    if(state[i] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING){
+        state[i]=EATING;
+        mostrar();
+        sem_post(sem_fil[i]);
+    }
+}
+void comer(int i){
+    float_rand=0.001*random();
+    int_rand=float_rand;
+    usleep(int_rand);
+}
+void por_garfo(int i){
+	sem_wait(mutex);
+	state[i]=THINKING;
+	mostrar();
+	test(LEFT);
+	test(RIGHT);
+	sem_post(mutex);
+}
