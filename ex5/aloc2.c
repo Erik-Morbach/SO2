5ea5d27b1d57bcf3b607ec2137ad5cb45c8282c0
Erik Morbach <morbacherik@gmail.com>
2026-06-18 22:07:12 -0300

    aloc2

---
+++ ex5/aloc2.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+int main() {
+ size_t fatia = 104857600; /* 100 MB */
+ void *pont;
+ for(;;) {
+ pont = malloc(fatia);
+ mlock(pont,fatia/1.1);
+ sleep(2);
+ }
+ return 0;
+}
