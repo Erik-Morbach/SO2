5ba0234fd5d6264c941d04f2341dfb8d0ab94737
Erik Morbach <morbacherik@gmail.com>
2026-06-25 22:27:54 -0300

    tlb idea

--- memorySim/src/tlb.hpp
+++ memorySim/src/tlb.hpp
@@ -1,9 +1,20 @@
 #pragma once
 #include <cstddef>
+#include <map>
+#include <unordered_map>
 
 #include "pageEntry.hpp"
+#include "fastSegTree.hpp"
 
 class TLB {
+private:
+	uint64_t timer;
+	FastSegTree<uint64_t> segTree;
+
+	std::vector<PageEntry> entries;
+
+	std::map<int, std::unordered_map<size_t, size_t>> pageMapper;
+	size_t pageNumberAndPidToIndex(size_t pageNumber);
 public:
 	bool exist(size_t pageNumber);
 	PageEntry get(size_t pageNumber);
